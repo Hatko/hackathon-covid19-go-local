98275b851aed28e53eab8cd8c2d50ef630ba34ab
Vlad Hatko <vladhatko@gmail.com>
2020-04-21 20:23:32 +0200

    Improve homescreen

--- Home.jsx
+++ Home.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, Image } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 
 export const HomeScreen = () => {
   const { navigate } = useNavigation()
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title='Business' style={{ marginBottom: 10 }} onPress={() => {
-        navigate('CompanySignIn')
-      }} />
-      <Button title='Customer' onPress={() => {
-        navigate('Map')
-      }} />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', paddingVertical: 80 }}>
+      <Text style={{ fontSize: 48 }}>
+        Go local
+      </Text>
+      <Image
+        source={require('./assets/logo.png')}
+        style={{
+          width: 245,
+          height: 200,
+          marginBottom: 40,
+        }}
+      />
+      <View style={{ alignContent: 'flex-end', width: '100%' }}>
+        <Button
+          title='Business'
+          style={{ marginBottom: 40 }}
+          containerStyle={{ width: '100%', paddingHorizontal: 40 }}
+          onPress={() => {
+            navigate('CompanySignIn')
+          }}
+        />
+        <Button
+          title='Customer'
+          containerStyle={{ width: '100%', paddingHorizontal: 40 }}
+          onPress={() => {
+            navigate('Map')
+          }} />
+      </View>
     </View>
   );
 }
