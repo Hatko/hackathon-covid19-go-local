a57c0d5465142ce2ebc61a7723ccafe3a27fd8a5
Ekamba <axelgassay@yahoo.dk>
2020-04-21 20:12:24 +0200

    add some changes in companySignIn

--- CompanyPage.jsx
+++ CompanyPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { View, Text } from "react-native";
+import { Button } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 export const CompanyPage = () => {
-  const { navigate } = useNavigation()
+  const { navigate } = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Company page</Text>
     </View>
   );
-}
+};
--- Root.jsx
+++ Root.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { ThemeProvider } from 'react-native-elements';
-import { HomeScreen } from './Home'
-import { CompanySignIn } from './CompanySignIn'
-import { CompanyPage } from './CompanyPage'
-import { Map } from './Map'
-import { Donation } from './Donation'
-import { UserSignIn } from './UserSignIn'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ThemeProvider } from "react-native-elements";
+import { HomeScreen } from "./Home";
+import { CompanySignIn } from "./CompanySignIn";
+import { CompanyPage } from "./CompanyPage";
+import { Map } from "./Map";
+import { Donation } from "./Donation";
+import { UserSignIn } from "./UserSignIn";
 
 const Stack = createStackNavigator();
 
 export const Root = () => {
-  return <NavigationContainer>
-    <ThemeProvider>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} options={{ header: () => null }} />
-        <Stack.Screen name='CompanySignIn' component={CompanySignIn} />
-        <Stack.Screen name='CompanyPage' component={CompanyPage} />
-        <Stack.Screen name='Map' component={Map} />
-        <Stack.Screen name='Donation' component={Donation} />
-        <Stack.Screen name='UserSignIn' component={UserSignIn} />
-      </Stack.Navigator>
-    </ThemeProvider>
-  </NavigationContainer>
-}
+  return (
+    <NavigationContainer>
+      <ThemeProvider>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ header: () => null }}
+          />
+          <Stack.Screen name="CompanySignIn" component={CompanySignIn} />
+          <Stack.Screen name="CompanyPage" component={CompanyPage} />
+          <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen name="Donation" component={Donation} />
+          <Stack.Screen name="UserSignIn" component={UserSignIn} />
+        </Stack.Navigator>
+      </ThemeProvider>
+    </NavigationContainer>
+  );
+};
