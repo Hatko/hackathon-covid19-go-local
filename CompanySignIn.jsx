4c36b5ab62551fb8147e768d8464d23fe564d7a0
Ekamba <axelgassay@yahoo.dk>
2020-04-21 21:14:35 +0200

    add some changes in companySignIn

--- CompanySignIn.jsx
+++ CompanySignIn.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
 import { Button } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 import { TextInput } from "react-native-gesture-handler";
@@ -8,70 +14,62 @@ export const CompanySignIn = () => {
   const { navigate } = useNavigation();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "black",
-      }}
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
     >
-      <Text style={{ color: "grey", fontWeight: "bold", fontSize: 30 }}>
-        Get paid now.
-      </Text>
-      <Text style={{ color: "grey", fontWeight: "bold", fontSize: 30 }}>
-        Serve later
-      </Text>
-      <View style={styles.action}>
-        <TextInput placeholder="CVR no" style={styles.textInput} />
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <Text style={{ color: "grey", fontWeight: "bold", fontSize: 30 }}>
+          Get paid now.
+        </Text>
+        <Text style={{ color: "grey", fontWeight: "bold", fontSize: 30 }}>
+          Serve later
+        </Text>
+        <View style={styles.action}>
+          <TextInput placeholder="CVR no" style={styles.textInput} />
+        </View>
+        <View style={styles.action}>
+          <TextInput placeholder="Name" style={styles.textInput} />
+        </View>
+        <View style={{ marginTop: "5%", width: "80%" }}>
+          <TouchableOpacity
+            style={{
+              borderBottomWidth: 1,
+              height: 42,
+              width: "80%",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 40,
+              backgroundColor: "#0097e6",
+              alignSelf: "center",
+              textAlign: "center",
+            }}
+          >
+            <Text style={{ color: "white" }}>Log In</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.action}>
-        <TextInput placeholder="Name" style={styles.textInput} />
-      </View>
-      <View style={{ marginTop: "5%", width: "80%" }}>
-        <TouchableOpacity
-          style={{
-            borderBottomWidth: 1,
-            height: 42,
-            width: "80%",
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: 40,
-            backgroundColor: "blue",
-            alignSelf: "center",
-            textAlign: "center",
-          }}
-        >
-          <Text style={{ color: "white" }}>Log In</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 var styles = StyleSheet.create({
   textInput: {
     flex: 1,
     marginTop: 2,
+    marginLeft: 10,
     paddingBottom: 2,
-    color: "grey",
+    color: "#57606f",
   },
   action: {
     flexDirection: "row",
     borderBottomWidth: 1,
     borderBottomColor: "#f2f2f2",
-  },
-
-  animation: {
-    backgroundColor: "#93278f",
-    paddingVertical: 10,
-    marginTop: 30,
-    borderRadius: 100,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textLogIn: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 18,
   },
 });
